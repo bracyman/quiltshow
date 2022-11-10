cda721e5b99600b18a98b7c28ab4b1e04295cb03
John Bracy <john.bracy@gmail.com>
2022-11-10 17:44:42 -0600

    updated react router dom version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import QuiltEntry from './quilts/QuiltEntryForm';
-import Header from './Header.js'
+import Header from './Header.js';
 
 class App extends Component {
   render() {
@@ -11,10 +11,10 @@ class App extends Component {
         <div>
           <Header/>
           <Router>
-            <Switch>
-              <Route path='/' exact={true} component={Home}/>
-              <Route path='/quiltEntry' exact={true} component={QuiltEntry}/>
-            </Switch>
+            <Routes>
+              <Route path='/' exact={true} element={<Home/>}/>
+              <Route path='/quiltEntry' exact={true} element={<QuiltEntry/>}/>
+            </Routes>
           </Router>
         </div>
     );
