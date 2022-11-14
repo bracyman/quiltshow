5145d6acf1efd9c5bc9dea4920cf48cbfb7a44e9
John Bracy <john.bracy@gmail.com>
2022-11-13 18:46:33 -0600

    Revamped UI, switched to useContext

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import QuiltEntry from './quilts/QuiltEntryForm';
-import Header from './Header.js';
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import QuiltList from "./components/quilts/QuiltList";
+import Header from "./Header.js";
+import QuiltContextProvider from "./contexts/QuiltContext";
 
-class App extends Component {
-  render() {
-    return (
-        <div>
-          <Header/>
-          <Router>
-            <Routes>
-              <Route path='/' exact={true} element={<Home/>}/>
-              <Route path='/quiltEntry' exact={true} element={<QuiltEntry/>}/>
-            </Routes>
-          </Router>
-        </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Header />
+      <QuiltContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" exact={true} element={<Home />} />
+            <Route path="/quiltEntry" exact={true} element={<QuiltList />} />
+          </Routes>
+        </Router>
+      </QuiltContextProvider>
+    </div>
+  );
 }
 
 export default App;
